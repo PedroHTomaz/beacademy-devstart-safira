c1cfd82c8f71b0b38ce87ff28eb96d45fa7ba9fe
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-06-27 17:34:18 -0300

    criacao do controller product, para receber a rota produtos. Estilizacao do form de cadastro, criacao de alguns diretorio e inclusao de arquivos

---
+++ projeto-empresarial/resources/views/layouts/template.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#3E065F">
+            <div class="container">
+                <a class="navbar-brand" href="#">LOJA SAFIRA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <nav class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">INÍCIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PRODUTOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">NOVO PRODUTO</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </header>
+
+    <article>
+        @yield('content')
+    </article>
+
+    <footer>
+
+    </footer>
+
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/js/scriptFormatCoin.js')}}"></script>
+</body>
+</html>
