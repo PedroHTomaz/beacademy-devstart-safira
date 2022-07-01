394e31389fbd238d8d15e10b8d439b7af9edff65
EleazarSF <eleazarsf@gmail.com>
2022-07-01 11:14:55 -0300

    home added

--- projeto-empresarial/routes/web.php
+++ projeto-empresarial/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 // Route::get('/produtos/novo', function () {
