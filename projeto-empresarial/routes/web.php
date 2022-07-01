c9499ab2d256f7354bb0c98f52a5251666ba04a1
Pedro Tomaz <pedrohtomaz2021@gmail.com>
2022-07-01 12:15:23 -0300

    Criar usuário

--- projeto-empresarial/routes/web.php
+++ projeto-empresarial/routes/web.php
@@ -3,18 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/produtos/novo', function () {
-//     return view('add-product');
-// });
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 
 Route::get('/produtos/novo',[ProductController::class,'add'])->name('produtos.add');
 Route::post('/produtos',[ProductController::class,'create'])->name('produtos.create');
-
 
 Route::get('/admin/login', function () {
     return view ('admin-login');
