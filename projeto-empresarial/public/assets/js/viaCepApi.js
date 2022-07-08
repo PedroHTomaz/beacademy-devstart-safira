27478b5c9c9c2140e846454f50d8cc09853a0b22
EleazarSF <eleazarsf@gmail.com>
2022-07-08 19:00:13 -0300

    api viacep implementada no cadastro de usuario

---
+++ projeto-empresarial/public/assets/js/viaCepApi.js
@@ -0,0 +1,51 @@
+function limpa_formulário_cep() {
+    document.getElementById('street').value=("");
+    document.getElementById('neighborhood').value=("");
+    document.getElementById('city').value=("");
+    document.getElementById('state').value=("");
+}
+
+function meu_callback(conteudo) {
+if (!("erro" in conteudo)) {
+    document.getElementById('street').value=(conteudo.logradouro);
+    document.getElementById('neighborhood').value=(conteudo.bairro);
+    document.getElementById('city').value=(conteudo.localidade);
+    document.getElementById('state').value=(conteudo.uf);
+}
+else {
+    limpa_formulário_cep();
+    alert("CEP não encontrado.");
+}
+}
+
+function pesquisacep(valor) {
+
+var cep = valor.replace(/\D/g, '');
+
+if (cep != "") {
+
+    var validacep = /^[0-9]{8}$/;
+
+    if(validacep.test(cep)) {
+
+        document.getElementById('street').value="...";
+        document.getElementById('neighborhood').value="...";
+        document.getElementById('city').value="...";
+        document.getElementById('state').value="...";
+
+        var script = document.createElement('script');
+
+        script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+        document.body.appendChild(script);
+
+    }
+    else {
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+    }
+}
+else {
+    limpa_formulário_cep();
+}
+};
