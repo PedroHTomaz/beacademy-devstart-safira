26087e59d57f8a8ba5ec795cc26149bf82898eac
EleazarSF <eleazarsf@gmail.com>
2022-07-16 17:38:06 -0300

    dando vida ao botao add ao carrinho

---
+++ projeto-empresarial/public/assets/js/cart.js
@@ -0,0 +1,4 @@
+function addToCart () {
+    let doc = document.getElementById('add');
+    doc.innerHTML = +doc.textContent + 1;
+  };
--- projeto-empresarial/resources/views/layouts/template-main.blade.php
+++ projeto-empresarial/resources/views/layouts/template-main.blade.php
@@ -13,6 +13,8 @@
 <body>
     @yield('content')
     <script src="{{asset('assets/js/viaCepApi.js')}}"></script>
+    <script src="{{asset('assets/js/cart.js')}}"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
