77ef7800d73ee207b4179dcda26db70847b32536
Felipe Erick <rick0531927@gmail.com>
2022-07-19 16:37:13 -0300

    deixei a foto opcional e coloquei um avatar

--- projeto-empresarial/app/Http/Controllers/Auth/RegisteredUserController.php
+++ projeto-empresarial/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -55,7 +55,7 @@ class RegisteredUserController extends Controller
             'city' => ['required'],
             'state' => ['required'],
             'cep' => ['required'],
-            'photo' => ['required'],
+            'photo' => ['file'],
         ]);
 
         $user = User::create([
