2d173881865d9b6c00d4eeab56739fc854e52919
EleazarSF <eleazarsf@gmail.com>
2022-07-20 20:37:14 -0300

    scrollbar consertada

--- projeto-empresarial/resources/views/product/checkout.blade.php
+++ projeto-empresarial/resources/views/product/checkout.blade.php
@@ -7,6 +7,7 @@
     <title>SAFIRA - Checkout</title>
 
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/scrollbar.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/form-validation.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
