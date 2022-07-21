76083fbec11d48e5e09e938b57d34dfe4db80dd9
Pedro Tomaz <pedrohtomaz2021@gmail.com>
2022-07-21 10:02:20 -0300

    Test/User Register e User Login

--- projeto-empresarial/database/factories/UserFactory.php
+++ projeto-empresarial/database/factories/UserFactory.php
@@ -5,32 +5,27 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
         return [
             'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'email' => $this->faker->email(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
+            'tel' => '85 8899 1122',
+            'birth_date' => '13/02/2004',
+            'cpf' => '01122233355',
+            'cep' => '60540660',
+            'street' => 'Rua Joao XXIII',
+            'number' => '0405',
+            'neighborhood' => 'Granja',
+            'city' => 'Fortaleza',
+            'state' => 'Ceará',
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
     public function unverified()
     {
         return $this->state(function (array $attributes) {
