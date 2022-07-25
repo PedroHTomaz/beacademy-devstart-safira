2aa345463d06dab2ad67c1c24ccf09d03d3128e2
Felipe Erick <rick0531927@gmail.com>
2022-07-25 15:47:36 -0300

    coloquei mais regras no form de cadastro de usuario e coloquei a tratativa de erros no form

--- projeto-empresarial/resources/views/users/create.blade.php
+++ projeto-empresarial/resources/views/users/create.blade.php
@@ -23,24 +23,31 @@
     <form action="{{ route('users.registered') }}" method="POST" enctype="multipart/form-data" class='rounded shadow p-3 p-md-5 text-start' style='background-color:#fff;'>
         @csrf
         <div>
+            @if($errors->any())
+                <div class='alert alert-dark' role="alert">
+                        @foreach($errors->all() as $error)
+                            {{ $error }}<br>
+                        @endforeach
+                </div>   
+            @endif
             <label for="" class='form-label text-primary h3'>Dados básicos</label>
-            <input type="text" id="name" name="name" placeholder='Nome completo' class='form-control mb-3' required>
-            <input type="email" id="email" name="email" placeholder='E-mail' class='form-control mb-3' required>
-            <input type="password" id="password" name="password" placeholder='Senha' class='form-control mb-3' required>
-            <input type="tel" id="tel" name="tel" placeholder='Telefone' class='form-control mb-3' required>
-            <input type="date" id="birth_date" name="birth_date" placeholder='Data de nascimento' class='form-control mb-3' required>
-            <input type="text" id="cpf" name="cpf" placeholder='CPF' class='form-control mb-3' required>
+            <input type="text" id="name" name="name" placeholder='Nome completo' class='form-control mb-3' >
+            <input type="email" id="email" name="email" placeholder='E-mail' class='form-control mb-3' >
+            <input type="password" id="password" name="password" placeholder='Senha' class='form-control mb-3' >
+            <input type="tel" id="tel" name="tel" placeholder='Telefone' class='form-control mb-3' >
+            <input type="date" id="birth_date" name="birth_date" max="1979-12-31" min="2000-01-02" placeholder='Data de nascimento'  class='form-control mb-3' >
+            <input type="text" id="cpf" name="cpf" placeholder='CPF' class='form-control mb-3' >
             <label for="photo" class="form-label">Selecione uma foto para o seu perfil.</label>
             <input type="file" id="photo" name="photo" class="form-control form control-md">
         </div>
         <div>
             <label for="" class='form-label text-primary h3'>Endereço</label>
-            <input type="text" id="cep" name="cep" placeholder='CEP' class='form-control mb-3' onblur="pesquisacep(this.value);" required>
-            <input type="text" id="street" name="street" placeholder='Rua' class='form-control mb-3' required>
-            <input type="text" id="number" name="number" placeholder='Número' class='form-control mb-3' required>
-            <input type="text" id="neighborhood" name="neighborhood" placeholder='Bairro' class='form-control mb-3' required>
-            <input type="text" id="city" name="city" placeholder='Cidade' class='form-control mb-3' required>
-            <input type="text" id="state" name="state" placeholder='Estado' class='form-control mb-3' required>
+            <input type="text" id="cep" name="cep" placeholder='CEP' class='form-control mb-3' onblur="pesquisacep(this.value);" >
+            <input type="text" id="street" name="street" placeholder='Rua' class='form-control mb-3' >
+            <input type="text" id="number" name="number" placeholder='Número' class='form-control mb-3' >
+            <input type="text" id="neighborhood" name="neighborhood" placeholder='Bairro' class='form-control mb-3' >
+            <input type="text" id="city" name="city" placeholder='Cidade' class='form-control mb-3' >
+            <input type="text" id="state" name="state" placeholder='Estado' class='form-control mb-3' >
         </div>
         <button type='submit' class='btn btn-primary d-block w-100'>Cadastrar</button>
     </form>
